ad93625fbaf34aef6bcfc1d3e6975a76df3a795c
Carlos José Díaz Baso <cada4215@zenith.astro.su.se>
2019-03-29 17:44:50 +0100

    model and simu weights added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# denoiser
-Solar image denoising with convolutional neural networks
+# Solar image denoising with convolutional neural networks
+
+We have designed a neural network that is capable of recovering weak signals under a complex noise corruption using deep-learning techniques.
+
+## Abstract
+
+The topology and dynamics of the solar chromosphere are dominated by magnetic fields. The latter can be inferred by analyzing polarimetric observations of spectral lines. However, polarimetric signals induced by chromospheric magnetic fields are particularly weak, and in most cases very close to the detection limit of current instrumentation. Therefore there are few observational studies that have successfully reconstructed the three components of the magnetic field vector in the chromosphere. Traditionally, the signal-to-noise ratio of observations has been improved by performing time-averages or spatial averages, but in both cases some information is lost. More advanced techniques, like Principal-component-analysis, have also been employed to take advantage of the sparsity of the observations in the spectral direction. In the present study, we propose to use the spatial coherence of the observations to reduce the noise using deep-learning techniques. We have designed a neural network that is capable of recovering weak signals under a complex noise corruption (including instrumental artifacts and non-linear post-processing). The training of the network is carried out without a priori knowledge of the clean signals, or an explicit statistical characterization of the noise or other corruption, only using the same observations as our generative model. Synthetic experiments are used to demonstrate the performance of this method. After that, we show examples of the improvement in typical signals obtained in current telescopes such as the Swedish 1-meter Solar Telescope.
---
+++ network/models.py
@@ -0,0 +1,145 @@
+from keras.layers import Input, Conv2D, Activation, BatchNormalization, GaussianNoise, add, UpSampling2D, Dropout, Concatenate, Merge, MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.models import Model
+from keras.regularizers import l2
+import tensorflow as tf
+from keras.engine.topology import Layer
+from keras.engine import InputSpec
+from keras.utils import conv_utils
+
+# ==================================================================================
+def spatial_reflection_2d_padding(x, padding=((1, 1), (1, 1)), data_format=None):
+    """Pads the 2nd and 3rd dimensions of a 4D tensor.
+    """
+    assert len(padding) == 2
+    assert len(padding[0]) == 2
+    assert len(padding[1]) == 2
+    if data_format is None:
+        data_format = image_data_format()
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('Unknown data_format ' + str(data_format))
+
+    if data_format == 'channels_first':
+        pattern = [[0, 0],
+                   [0, 0],
+                   list(padding[0]),
+                   list(padding[1])]
+    else:
+        pattern = [[0, 0],
+                   list(padding[0]), list(padding[1]),
+                   [0, 0]]
+    return tf.pad(x, pattern, "REFLECT")
+
+
+
+
+
+# ==================================================================================
+class ReflectionPadding2D(Layer):
+    """Reflection-padding layer for 2D input (e.g. picture).
+    This layer can add rows and columns or zeros
+    at the top, bottom, left and right side of an image tensor.
+    """
+    
+    def __init__(self,
+                 padding=(1, 1),
+                 data_format=None,
+                 **kwargs):
+        super(ReflectionPadding2D, self).__init__(**kwargs)
+        self.data_format = conv_utils.normalize_data_format(data_format)
+        if isinstance(padding, int):
+            self.padding = ((padding, padding), (padding, padding))
+        elif hasattr(padding, '__len__'):
+            if len(padding) != 2:
+                raise ValueError('`padding` should have two elements. '
+                                 'Found: ' + str(padding))
+            height_padding = conv_utils.normalize_tuple(padding[0], 2,
+                                                        '1st entry of padding')
+            width_padding = conv_utils.normalize_tuple(padding[1], 2,
+                                                       '2nd entry of padding')
+            self.padding = (height_padding, width_padding)
+        else:
+            raise ValueError('`padding` should be either an int, '
+                             'a tuple of 2 ints '
+                             '(symmetric_height_pad, symmetric_width_pad), '
+                             'or a tuple of 2 tuples of 2 ints '
+                             '((top_pad, bottom_pad), (left_pad, right_pad)). '
+                             'Found: ' + str(padding))
+        self.input_spec = InputSpec(ndim=4)
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_first':
+            if input_shape[2] is not None:
+                rows = input_shape[2] + self.padding[0][0] + self.padding[0][1]
+            else:
+                rows = None
+            if input_shape[3] is not None:
+                cols = input_shape[3] + self.padding[1][0] + self.padding[1][1]
+            else:
+                cols = None
+            return (input_shape[0],
+                    input_shape[1],
+                    rows,
+                    cols)
+        elif self.data_format == 'channels_last':
+            if input_shape[1] is not None:
+                rows = input_shape[1] + self.padding[0][0] + self.padding[0][1]
+            else:
+                rows = None
+            if input_shape[2] is not None:
+                cols = input_shape[2] + self.padding[1][0] + self.padding[1][1]
+            else:
+                cols = None
+            return (input_shape[0],
+                    rows,
+                    cols,
+                    input_shape[3])
+
+    def call(self, inputs):
+        return spatial_reflection_2d_padding(inputs,
+                                    padding=self.padding,
+                                    data_format=self.data_format)
+
+    def get_config(self):
+        config = {'padding': self.padding,
+                  'data_format': self.data_format}
+        base_config = super(ReflectionPadding2D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+
+
+
+# ==================================================================================
+def unet(start_ch=32, depth=2, activation='relu', input_channel_num=1, out_ch=1, inc_rate=2., 
+        dropout=0.0, batchnorm=False, maxpool=True, upconv=True, residual=False):
+    # UNet: code from https://github.com/pietz/unet-keras
+
+    def _conv_block(m, dim, acti, bn, res, do=0):
+        n = ReflectionPadding2D()(m)
+        n = Conv2D(dim, 3, padding='valid',kernel_initializer='he_normal',activation=acti)(n)
+        n = ReflectionPadding2D()(n)
+        n = Conv2D(dim, 3, padding='valid',kernel_initializer='he_normal',activation=acti)(n)
+        return Concatenate()([m, n]) if res else n
+
+    def _level_block(m, dim, depth, inc, acti, do, bn, mp, up, res):
+        if depth > 0:
+            n = _conv_block(m, dim, acti, bn, res)
+            m = MaxPooling2D()(n) if mp else Conv2D(dim, 3, strides=2, padding='same')(n)
+            m = _level_block(m, int(inc * dim), depth - 1, inc, acti, do, bn, mp, up, res)
+            if up:
+                m = UpSampling2D()(m)
+                m = Conv2D(dim, 2, activation=acti, padding='same',kernel_initializer='he_normal')(m)
+            else:
+                m = Conv2DTranspose(dim, 3, strides=2, activation=acti, padding='same')(m)
+            n = Concatenate()([n, m])
+            m = _conv_block(n, dim, acti, bn, res)
+        else:
+            m = _conv_block(m, dim, acti, bn, res, do)
+        return m
+
+    i = Input(shape=(None, None, input_channel_num))
+    o = _level_block(i, start_ch, depth, inc_rate, activation, dropout, batchnorm, maxpool, upconv, residual)
+    o = Conv2D(out_ch, 1)(o)
+    return Model(inputs=i, outputs=o)
+
